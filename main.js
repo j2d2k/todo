dd850100b14326c1bcae67ee57043e21bd3e64ee
j2d2k <jafardawood015@gmail.com>
2024-06-26 09:13:32 +0300

    updating

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-function myFunction() {
-document.getElementsByClassName("grow2").innerhtml=(form2);
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const button1 = document.getElementById('button1');
+    const button2 = document.getElementById('button2');
+    const form1 = document.getElementById('form1');
+    const form2 = document.getElementById('form2');
+
+    button1.addEventListener('click', () => {
+        form1.style.display = 'block';
+        form2.style.display = 'none';
+        button1.classList.add('grow');
+        button2.classList.remove('grow');
+    });
+
+    button2.addEventListener('click', () => {
+        form2.style.display = 'block';
+        form1.style.display = 'none';
+        utton2.classList.add('grow');
+        button1.classList.remove('grow');
+    });
+});
+
